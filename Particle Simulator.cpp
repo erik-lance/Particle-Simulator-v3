e89275d0b1ace0348737531879b60af6ed8a5e02
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-01-28 12:42:53 +0800

    Add project files.

---
+++ Particle Simulator.cpp
@@ -0,0 +1,12 @@
+﻿// Particle Simulator.cpp : Defines the entry point for the application.
+//
+
+#include "Particle Simulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
