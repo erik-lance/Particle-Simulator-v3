32f22ad02516db68313a18e18db8b9e7e7f59a8d
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-07 02:14:07 +0800

    Added initial grid cell

--- CollisionManager.cpp
+++ CollisionManager.cpp
@@ -9,3 +9,29 @@ CollisionManager::CollisionManager(int width, int height)
 CollisionManager::~CollisionManager()
 {
 }
+
+/**
+ * Sets up the grid dimensions, cell sizes, and initializes the grid
+ * @param columns The number of columns in the grid
+ * @param rows The number of rows in the grid
+ */
+void CollisionManager::setGridDimensions(int columns, int rows)
+{
+	grid_columns = columns;
+	grid_rows = rows;
+
+	grid_cell_width = simulator_width / grid_columns;
+	grid_cell_height = simulator_height / grid_rows;
+
+	// Initialize the grid
+}
+
+/**
+ * Gets the grid cell given the particle's position
+ * @param x The particle's x position
+ * @param y The particle's y position
+ */
+std::pair<int, int> CollisionManager::getGridCell(int x, int y) 
+{
+
+}
--- CollisionManager.h
+++ CollisionManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 
 /**
  * The CollisionManager splits the simulation into a grid and checks for collisions within the grid.
@@ -11,12 +12,15 @@ public:
 	~CollisionManager();
 
 	void setSimulatorDimensions(int width, int height) { simulator_width = width; simulator_height = height; }
-	void setGridDimensions(int columns, int rows) { grid_columns = columns; grid_rows = rows; }
-	void setGridCellDimensions(int width, int height) { grid_cell_width = width; grid_cell_height = height; }
+	void setGridDimensions(int columns, int rows);
+
+	std::pair<int, int> getGridCell(int x, int y);
 
 private:
 	int simulator_width, simulator_height;
 	int grid_columns, grid_rows;
 	int grid_cell_width, grid_cell_height;
 
+	// Grid
+	std::vector<std::vector<int>> grid;
 };
