1af291f9ba1c2102b56ccc662e92b53d563f4002
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-09 20:34:29 +0800

    Added = operator for position

--- Structures.h
+++ Structures.h
@@ -3,3 +3,8 @@
 struct Position { int x; int y; };
 struct Line  { Position start; Position end; };
 struct Screen { int width = 1280; int height = 720; };
+
+bool operator==(const Position& lhs, const Position& rhs)
+{
+	return lhs.x == rhs.x && lhs.y == rhs.y;
+}
