63db8de445799f58379fa0beb1722a9d438ab04c
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-09 14:35:06 +0800

    Wall has line data structure

--- Objects/Wall.h
+++ Objects/Wall.h
@@ -1,5 +1,12 @@
 #pragma once
 
+struct Line {
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+};
+
 class Wall {
 public:
 	Wall(int id);
@@ -7,10 +14,12 @@ public:
 	~Wall();
 
 	int getID() const { return m_id; }
+	Line getLine() const { return m_line; }
 
 	void setID(int id) { m_id = id; }
 
 private:
 	int m_id;
+	Line m_line;
 
 };
