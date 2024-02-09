214c4be4467352c4f95d3ea53333c4dfdddc9ff6
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-09 12:08:46 +0800

    Added initial wall skeleton

--- Objects/Wall.h
+++ Objects/Wall.h
@@ -1,5 +1,26 @@
 #pragma once
 
 class Wall {
+public:
+	Wall(int id);
+	Wall();
+	~Wall();
+
+	int getID() const { return m_id; }
+
+	void setID(int id) { m_id = id; }
+
+private:
+	int m_id;
 
 };
+
+Wall::Wall(int id) {
+	m_id = id;
+}
+
+Wall::Wall() {
+}
+
+Wall::~Wall() {
+}
