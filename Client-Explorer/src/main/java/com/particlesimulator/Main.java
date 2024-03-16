035893e66e5d7cdf4cb91fca4c356790f3c232b6
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-03-16 13:52:12 +0800

    Updated java client to a maven project

--- Client-Explorer/src/main/java/com/particlesimulator/Main.java
+++ Client-Explorer/src/main/java/com/particlesimulator/Main.java
@@ -1,6 +1,5 @@
-/**
- * Start point of the program.
- */
+package com.particlesimulator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
