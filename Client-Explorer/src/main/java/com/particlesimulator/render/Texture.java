7bc5aaab97c27b3e6ff8f0518fd681cea1a33390
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-03-17 20:25:28 +0800

    Updated texture to use Utils

--- Client-Explorer/src/main/java/com/particlesimulator/render/Texture.java
+++ Client-Explorer/src/main/java/com/particlesimulator/render/Texture.java
@@ -15,6 +15,8 @@ import javax.imageio.ImageIO;
 
 import org.lwjgl.BufferUtils;
 
+import com.particlesimulator.Utils;
+
 /**
  * Represents a texture in the game.
  */
@@ -29,51 +31,11 @@ public class Texture {
      * @param filename
      */
     public Texture(String filename) {
-        BufferedImage bufferedImage;
+        textureId = Utils.glLoadTexture(filename);
 
-        try {
-            // Load the texture from the file
-            URI file = getClass().getResource("/sprites/"+filename).toURI();
-            bufferedImage = ImageIO.read(new File(file));
-            width = bufferedImage.getWidth();
-            height = bufferedImage.getHeight();
-
-            // Create a new array of pixels
-            int[] pixels = new int[width * height];
-            bufferedImage.getRGB(0, 0, width, height, null, 0, width);
-
-            // Create a new byte buffer to store the pixel data
-            ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * 4);
-
-            // Loop through each pixel and add it to the buffer
-            for (int y = 0; y < height; y++) { // Loop through each row of pixels
-                for (int x = 0; x < width; x++) { // Loop through each pixel in the row
-                    int pixel = pixels[y * width + x];
-                    buffer.put((byte) ((pixel >> 16) & 0xFF)); // Red
-                    buffer.put((byte) ((pixel >> 8) & 0xFF)); // Green
-                    buffer.put((byte) (pixel & 0xFF)); // Blue
-                    buffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha
-                }
-            }
-
-            // Flip the buffer so it can be read
-            buffer.flip();
-
-            // Generate a new texture ID
-            textureId = glGenTextures();
-
-            // Bind the texture
-            glBindTexture(GL_TEXTURE_2D, textureId);
-
-            // Set the texture parameters
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-            // Load the texture data
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-        }   catch (IOException | URISyntaxException e) {
-			e.printStackTrace();
-        }
+        // Default size in the 33x19 grid of sprites
+        width = 39;
+        height = 38;
     } 
 
     /**
@@ -84,15 +46,13 @@ public class Texture {
         glDeleteTextures(textureId);
         super.finalize();
     }
+    
+    public int getId() { return textureId; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
 
-    /**
-     * Bind the texture to the given sampler.
-     * @param sampler
-     */
-    public void bind(int sampler) {
-        if (sampler >= 0 && sampler <= 31) {
-            glActiveTexture(GL_TEXTURE0 + sampler);
-            glBindTexture(GL_TEXTURE_2D, textureId);
-        }
+    public void updateTexture(String filename) {
+        glDeleteTextures(textureId);
+        textureId = Utils.glLoadTexture(filename);
     }
 }
