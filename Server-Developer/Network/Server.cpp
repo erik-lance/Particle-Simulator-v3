88dbc28a2b55e5309cd53ffa01d94c9e042c9705
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-03-25 02:10:20 +0800

    Added initial server code

--- Server-Developer/Network/Server.cpp
+++ Server-Developer/Network/Server.cpp
@@ -1,2 +1,89 @@
 #include "Server.h"
 
+Server::Server(std::string host, int port, ObjectManager* object_manager)
+{
+	this->host = host;
+	this->port = port;
+	this->object_manager = object_manager;
+
+	// If windows, initialize winsock
+	#ifdef _WIN32
+		WSADATA wsa_data;
+		if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+		{
+			std::cerr << "Error initializing Winsock" << std::endl;
+			exit(1);
+		}
+	#endif
+
+	init();
+}
+
+Server::~Server()
+{
+	this->running = false;
+}
+
+/**
+ * Prepares the socket and server address
+ * */
+void Server::init()
+{
+	// Create a socket
+	this->m_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_socket < 0)
+	{
+		std::cerr << "Error creating socket" << std::endl;
+		exit(1);
+	}
+
+	// Set up the server address
+	memset((char*)&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+	InetPtonA(AF_INET, host.c_str(), &server_address.sin_addr); // Convert the host address to a usable format
+
+	// Set socket to reuse address
+	int opt = 1;
+	if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) != 0)
+	{
+		// Print full error details
+		char error[1024];
+		strerror_s(error, sizeof(error), errno);
+		std::cerr << "Error setting socket options: " << error << std::endl;
+		exit(1);
+	}
+
+	// Set socket to non-blocking
+
+	// Bind the socket to the server address
+	if (bind(m_socket, (struct sockaddr*)&server_address, sizeof(server_address)) != 0)
+	{
+		// Print full error details
+		char error[1024];
+		strerror_s(error, sizeof(error), errno);
+		std::cerr << "Error binding socket: " << error << std::endl;
+		exit(1);
+	}
+}
+
+void Server::start()
+{
+	// Start threads
+	receiver_thread = std::thread(&Server::receiver, this);
+	processor_thread = std::thread(&Server::processor, this);
+}
+
+void Server::receiver()
+{
+	while (running) {
+
+	}
+}
+
+void Server::processor()
+{
+	while (running) {
+
+	}
+}
--- Server-Developer/Network/Server.h
+++ Server-Developer/Network/Server.h
@@ -1,5 +1,51 @@
 #pragma once
+#include "../Objects/ObjectManager.h"
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#endif
 
 class Server {
+public:
+	Server(std::string host, int port, ObjectManager* object_manager);
+	~Server();
+	void init();
+	void start();
+	void receiver();
+	void processor();
+private:
+	std::string host;
+	int port;
+	std::mutex mtx; // to lock the messages queue
 
+	ObjectManager* object_manager;
+	SOCKET m_socket;
+	struct sockaddr_in server_address;
+
+	// Connections
+	std::vector<SOCKET> clients;
+
+	// Queue
+	std::queue<std::string> messages; // to send
+	std::queue <std::string> responses; // received
+
+	// Threads
+	bool running = true;
+	std::thread receiver_thread;
+	std::thread processor_thread;
 };
